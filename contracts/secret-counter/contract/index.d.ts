ee6645f29f7bcdfff64ef2185dce895d495c2163
hade <liveithade@gmail.com>
2026-03-25 17:24:27 +0700

    build: recompile contracts with compactc v0.30.0 (runtime 0.15.0)
    - Update language pragma to >= 0.22
    - Recompile counter and secret-counter for ledger-v8

--- contracts/secret-counter/contract/index.d.ts
+++ contracts/secret-counter/contract/index.d.ts
@@ -10,6 +10,12 @@ export type ImpureCircuits<PS> = {
   decrement(context: __compactRuntime.CircuitContext<PS>, amount_0: bigint): __compactRuntime.CircuitResults<PS, []>;
 }
 
+export type ProvableCircuits<PS> = {
+  init(context: __compactRuntime.CircuitContext<PS>, hash_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+  increment(context: __compactRuntime.CircuitContext<PS>, amount_0: bigint): __compactRuntime.CircuitResults<PS, []>;
+  decrement(context: __compactRuntime.CircuitContext<PS>, amount_0: bigint): __compactRuntime.CircuitResults<PS, []>;
+}
+
 export type PureCircuits = {
 }
 
@@ -32,6 +38,7 @@ export declare class Contract<PS = any, W extends Witnesses<PS> = Witnesses<PS>>
   witnesses: W;
   circuits: Circuits<PS>;
   impureCircuits: ImpureCircuits<PS>;
+  provableCircuits: ProvableCircuits<PS>;
   constructor(witnesses: W);
   initialState(context: __compactRuntime.ConstructorContext<PS>): __compactRuntime.ConstructorResult<PS>;
 }
